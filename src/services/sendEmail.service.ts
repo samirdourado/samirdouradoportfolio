f47b94ce02e0cf119edfe30a1ab6734aaec6a512
samirdourado <samir3500@gmail.com>
2026-07-03 01:54:08 -0300

    Feat: phone mask with regex

--- src/services/sendEmail.service.ts
+++ src/services/sendEmail.service.ts
@@ -1,17 +1,24 @@
 import { SendEmail } from "@/interfaces/sendEmail.interface"
 import emailjs from "@emailjs/browser"
 import { toast } from "react-toastify"
+const serviceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID
+const apiKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
 
 const sendEmail = async (formData: SendEmail) => {
 
-    const templateParams  = {
-        from_name: formData.name,
-        phone: formData.phone,
-        email: formData.email,
-        message: formData.text
-      };
+  if (!serviceId || !apiKey) {
+    toast.error("Tente outra forma de contato");
+    return;
+  }
 
-      await emailjs.send("service_svg6xev", "portfolio-contato", templateParams, "YQ1eb23BlZct2MFH4")
+  const templateParams  = {
+    from_name: formData.name,
+    phone: formData.phone,
+    email: formData.email,
+    message: formData.text
+  };
+
+      await emailjs.send(serviceId, "portfolio-contato", templateParams, apiKey)
       .then(() => {          
           toast.success("Email enviado")
         }, (error) => {
